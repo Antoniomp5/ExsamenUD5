5a73516daf812b784ac030eeef4e69d7a0b2a065
Antonio Hernández Gilabert <antoniohg2001@gmail.com>
2026-03-17 14:55:28 +0100

    Main.java

--- ExamenUD5/src/biblioteca/app/Main.java
+++ ExamenUD5/src/biblioteca/app/Main.java
@@ -1,13 +1,38 @@
 package biblioteca.app;
 
+import biblioteca.model.Autor;
 import biblioteca.model.Libro;
+import biblioteca.model.Revista;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Libro l1 = new Libro("IlloJuan", 25);
 		
+		//int item = 0;
+		
+		Autor a1 = new Autor ("IlloJuan", "español");
+		Autor a2 = new Autor ("Rubius", "noruego");
+		Autor a3 = new Autor ("Grefg", "andorrano");
+		
+		Libro l1 = new Libro(a1, 78);
+		Libro l2 = new Libro(a2, 178);
+		Libro l3 = new Libro(a3, 878);
+		
+		l1.obtenerDescripcion();
+		l2.obtenerDescripcion();
+		l3.obtenerDescripcion();
+		
+		Revista r1 = new Revista(34, "Semanal");
+		Revista r2 = new Revista(12, "Anual");
+		Revista r3 = new Revista(1000, "Diaría");
+		
+		r1.obtenerDescripcion();
+		r2.obtenerDescripcion();
+		r3.obtenerDescripcion();
+		
+		//item.getTotalItems();
 
 	}
 
 }
+
